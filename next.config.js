86519ad99422c6b73d51836066f1fa3e025b8ada
safayetaxilsoft <safayet@axilsoft.com>
2024-08-25 10:48:00 +0600

    image update

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'admin.axilsoft.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+    webpack(config, options) {
+      // Add a rule for SVG files
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
