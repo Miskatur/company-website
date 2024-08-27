8ae3bb11e7f31b395bd2b12042d82625cc682998
safayetaxilsoft <safayet@axilsoft.com>
2024-08-27 15:12:55 +0600

    update

--- src/components/pages/about/FirstBlock/FirstBlock.jsx
+++ src/components/pages/about/FirstBlock/FirstBlock.jsx
@@ -9,14 +9,14 @@ const FirstBlock = ({ data, imageurl }) => {
   const {
     about_first_section_title = "Providing the best service In digital marketing",
     about_first_section_description = "Contrasted dissimilar get joy you instrument out reasonably. Again keeps at no meant stuff. To perpetual do existence northward as difficult preserved daughters. Continued at up to zealously necessary breakfa-solidt. Surrounded sir motionless she end literature. Gay direction neglected but supported yet her.",
-    about_first_section_image_path
+    about_first_section_image_path,
   } = data || {};
 
-
-  const imagePath = !data || Object.keys(data).length === 0 || !about_first_section_image_path
-    ? "/img/axil-custom/1.svg"  
-    : `${imageurl}${about_first_section_image_path}`;
-
+  const imagePath =
+    !data || Object.keys(data).length === 0 || !about_first_section_image_path
+      ? "/img/axil-custom/1.svg"
+      : `${imageurl}${about_first_section_image_path}`;
+  console.log('imagePath',imagePath);
   return (
     <>
       <div className="about-style-two-area global-padding default-padding-bottom">
@@ -39,11 +39,13 @@ const FirstBlock = ({ data, imageurl }) => {
             <div className="col-lg-6 about-style-two pl-50 pl-md-15 pl-xs-15 mt-60 mt-xs-40 ps-lg-5 d-flex flex-column justify-content-center align-items-start">
               <div className="about-two-info ">
                 <h4 className="sub-title">About our company</h4>
-                <h2 className="title">
-                  {about_first_section_title}
-                </h2>
+                <h2 className="title">{about_first_section_title}</h2>
 
-                <p dangerouslySetInnerHTML={{ __html: about_first_section_description }} />
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: about_first_section_description,
+                  }}
+                />
 
                 <div className="about-grid-info">
                   <Link
