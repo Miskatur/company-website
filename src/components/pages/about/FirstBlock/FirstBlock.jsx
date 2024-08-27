be551028c3582ff31f65aaa8a913db5f84d9627f
safayetaxilsoft <safayet@axilsoft.com>
2024-08-27 16:06:01 +0600

    update

--- src/components/pages/about/FirstBlock/FirstBlock.jsx
+++ src/components/pages/about/FirstBlock/FirstBlock.jsx
@@ -1,4 +1,4 @@
-"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
@@ -11,7 +11,7 @@ const FirstBlock = ({ data, imageurl }) => {
     about_first_section_description = "Contrasted dissimilar get joy you instrument out reasonably. Again keeps at no meant stuff. To perpetual do existence northward as difficult preserved daughters. Continued at up to zealously necessary breakfa-solidt. Surrounded sir motionless she end literature. Gay direction neglected but supported yet her.",
     about_first_section_image_path,
   } = data || {};
-
+  
   const imagePath =
     !data || Object.keys(data).length === 0 || !about_first_section_image_path
       ? "/img/axil-custom/1.svg"
@@ -23,14 +23,14 @@ const FirstBlock = ({ data, imageurl }) => {
         <div className="">
           <div className="row">
             <div className="col-lg-6 about-style-two pe-lg-6">
-              {/* <div className=""> */}
+              
               <Image
                 src={imagePath}
                 alt="Image Not Found"
                 height={500}
-                width={500}
+                width={600}
               />
-              {/* </div> */}
+              
             </div>
             <div className="col-lg-6 about-style-two pl-50 pl-md-15 pl-xs-15 mt-60 mt-xs-40 ps-lg-5 d-flex flex-column justify-content-center align-items-start">
               <div className="about-two-info ">
