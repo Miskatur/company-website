f28eee28d6985387ddede7f2d730ac06e4b1b492
Miskatur <rahman@lintechgroup.co.uk>
2024-08-29 11:37:21 +0600

    team members picture url updated and condition added for no team member

--- src/components/pages/home/team/singleTeam.jsx
+++ src/components/pages/home/team/singleTeam.jsx
@@ -5,23 +5,30 @@ import React from "react";
 const SingleTeam = ({ team, imageurl }) => {
   const { name, designation, image_path } = team;
 
-  const imagePath = !team || Object.keys(team).length === 0 || !image_path
-    ? `img/team/1.jpg`  
-    : `${imageurl}${image_path}`;
-
-
+  const imagePath =
+    !team || Object.keys(team).length === 0 || !image_path
+      ? `/img/team/1.jpg`
+      : `${imageurl}${image_path}`;
 
   return (
     <>
       <div className="team-style-two">
         <div className="thumb">
-          <Image src={imagePath} alt="Image Not Found" width={300} height={370} />
+          <Image
+            src={`${imageurl}${image_path}`}
+            alt="Image Not Found"
+            width={300}
+            height={370}
+            style={{ color: "transparent" }}
+          />
           <div className="team-info">
             <div className="content">
               <h5>
-                <Link href="#">{name.length > 17 ? `${name.slice(0, 14)}...` : name}</Link>
+                <Link href="#">
+                  {name?.length > 17 ? `${name?.slice(0, 14)}...` : name}
+                </Link>
               </h5>
-              <span style={{fontSize:'14px'}}>{designation}</span>
+              <span style={{ fontSize: "14px" }}>{designation}</span>
             </div>
           </div>
         </div>
--- src/components/pages/home/team/team.jsx
+++ src/components/pages/home/team/team.jsx
@@ -9,9 +9,7 @@ const Team = ({ bgColor }) => {
   const TeamMember = data?.data?.team_member_list || [];
   const PageData = data?.data?.page_data;
 
-  const {
-    about_fourth_section_title = "Our team members",
-  } = PageData || {};
+  const { about_fourth_section_title = "Our team members" } = PageData || {};
 
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -21,44 +19,45 @@ const Team = ({ bgColor }) => {
         bgColor ? bgColor : ""
       } team-style-two-area default-padding-top pb-70 pb-xs-0`}
     >
-      <div className="container">
-        <div className="row align-center">
-          <div className="col-xl-6 col-lg-12">
-            <div className="site-heading">
-              <h5 className="sub-title">Team members</h5>
-              <h2 className="title">
-                {about_fourth_section_title}
-              </h2>
+      {TeamMember && (
+        <div className="container">
+          <div className="row align-center">
+            <div className="col-xl-6 col-lg-12">
+              <div className="site-heading">
+                <h5 className="sub-title">Team members</h5>
+                <h2 className="title">{about_fourth_section_title}</h2>
+              </div>
             </div>
+            {TeamMember?.length > 0 &&
+              TeamMember?.slice(0, 5)?.map((team) => (
+                <motion.div
+                  key={team?.id}
+                  initial="hidden"
+                  animate="visible"
+                  variants={{
+                    hidden: {
+                      opacity: 0,
+                      y: 50,
+                    },
+                    visible: {
+                      opacity: 1,
+                      y: 0,
+                      transition: {
+                        type: "spring",
+                        stiffness: 100,
+                        damping: 10,
+                      },
+                    },
+                  }}
+                  transition={{ delay: 0.2 }}
+                  className="col-xl-3 col-lg-4 col-md-6 mb-50"
+                >
+                  <SingleTeam team={team} imageurl={baseUrl} />
+                </motion.div>
+              ))}
           </div>
-          {TeamMember.length > 0 && TeamMember.slice(0, 5).map((team) => (
-            <motion.div
-              key={team.id}
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  y: 50,
-                },
-                visible: {
-                  opacity: 1,
-                  y: 0,
-                  transition: {
-                    type: "spring",
-                    stiffness: 100,
-                    damping: 10,
-                  },
-                },
-              }}
-              transition={{ delay: 0.2 }}
-              className="col-xl-3 col-lg-4 col-md-6 mb-50"
-            >
-              <SingleTeam team={team} imageurl={baseUrl} />
-            </motion.div>
-          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 };
